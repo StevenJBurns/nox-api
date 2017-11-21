df5e96a15761e881ac8c46185d869cc954bc8b22
SJB <StevenJBurns@users.noreply.github.com>
2017-11-21 08:57:20 -0700

    Commented out broken Facebook methods in Rails API

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,34 @@
 class ChargesController < ApplicationController
   #before_action :amount_to_be_charged
 
-  # def new
-  # end
-  #
-  # def create
-  #   @amount = 500
-  #
-  #   customer = StripeTool.create_customer(email: params[:stripeEmail],
-  #                                         stripe_token: params[:stripeToken])
-  #
-  #   charge = StripeTool.create_charge(customer_id: customer.id,
-  #                                     amount: @amount,
-  #                                     description: 'Rails Stripe customer')
-  #
-  # rescue Stripe::CardError => e
-  #   flash[:error] = e.message
-  #   redirect_to new_charge_path
-  # end
-  #
-  # def thanks
-  # end
+  def create
+    token = params[:token]
+    amount = params[:amount]
+
+    puts 'Hi from the server'
+    puts params[:token]
+
+    # Set your secret key: remember to change this to your live secret key in production
+    # See your keys here: https://dashboard.stripe.com/account/apikeys
+    Stripe.api_key = "sk_test_3nepeTzuSdUMQm9JKWF2fKCq"
+
+    # Token is created using Checkout or Elements!
+    # Get the payment token ID submitted by the form:
+
+
+    # Charge the user's card:
+    charge = Stripe::Charge.create(
+      :amount => amount,
+      :currency => "usd",
+      #:description => "Example charge",
+      :source => token,
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+  def thanks
+  end
 end
