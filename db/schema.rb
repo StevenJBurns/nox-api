25b90053e36af6798de2382ed417604852bdd7c0
SJB <StevenJBurns@users.noreply.github.com>
2017-11-22 11:30:39 -0700

    Seed and Migration changes; size added to Products table; NSFW Seeds removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120203207) do
+ActiveRecord::Schema.define(version: 20171122173904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171120203207) do
     t.integer "stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
   end
 
   create_table "services", force: :cascade do |t|
