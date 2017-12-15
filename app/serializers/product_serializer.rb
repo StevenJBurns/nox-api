cda1649aa0e7bf22bde7d03a8e500677a95f02df
SJB <StevenJBurns@users.noreply.github.com>
2017-12-15 15:41:11 -0700

    Updated serializer for Product

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :product_type, :design, :gender, :price, :stock_quantity
+  attributes :id, :product_type, :design, :gender, :price, :stock_quantity, :front_img, :back_img
 end
