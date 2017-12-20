e7a265e6f487b331281aa431239406dfe65c2f6c
SJB <StevenJBurns@users.noreply.github.com>
2017-12-20 16:24:19 -0700

    Added address to charge controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,13 +4,14 @@ class ChargesController < ApplicationController
   def create
     token = params[:token]
     amount = params[:amount]
+    address = params[:address]
 
     puts 'Hi from the server'
     puts params[:token]
 
     # Set your secret key: remember to change this to your live secret key in production
     # See your keys here: https://dashboard.stripe.com/account/apikeys
-    Stripe.api_key = "sk_test_3nepeTzuSdUMQm9JKWF2fKCq"
+    Stripe.api_key = "sk_test_oeXe9v0Dx57ooSxUJCMJzcAs"
 
     # Token is created using Checkout or Elements!
     # Get the payment token ID submitted by the form:
@@ -22,6 +23,7 @@ class ChargesController < ApplicationController
       :currency => "usd",
       #:description => "Example charge",
       :source => token,
+      :address => address
     )
 
   rescue Stripe::CardError => e
