d35586081dbb1dbac0c4fe42527bda4e61c27cce
SJB <StevenJBurns@users.noreply.github.com>
2017-12-21 15:56:31 -0700

    Adding shopping cart total check part 10

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -17,7 +17,7 @@ class ChargesController < ApplicationController
     # Get the payment token ID submitted by the form:
 
     @shoppingCarts = ShoppingCart.where(:user_id => currentUser)
-    puts 'Current Shopping Carts', json: @shoppingCarts.each
+    @shoppingCarts.each do { |e| puts e.quantity }
 
     # Charge the user's card:
     charge = Stripe::Charge.create(
